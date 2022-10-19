0273d4d71f7d91eca06fd77ce5d7800a95575707
loveth007 <lovethokwudiri@gmail.com>
2022-10-19 10:01:10 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include<stdio.h>
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,8 +15,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char c);
 void print_times_table(int n);
 
-
-#endif
+#endif /* MAIN_H */
